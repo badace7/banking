a7f9253fa66d4b5c3bde5b272febec86012e6510
badace <ali.belhacene@yahoo.fr>
2022-12-24 14:09:34 +0100

    add some methods to the repository port

--- src/domain/account/_ports/account.irepository.ts
+++ src/domain/account/_ports/account.irepository.ts
@@ -2,4 +2,6 @@ import AccountDomain from '../domain/account.domain';
 
 export interface IAccountRepository {
   findBankAccount(accountNumber: string): Promise<AccountDomain>;
+  saveBankAccount(account: AccountDomain): Promise<void>;
+  updateBankAccount(accountId: string, account: AccountDomain): Promise<void>;
 }
