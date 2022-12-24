03c8ee7c586e5a083c647bf9fc95580662ce18b2
badace <ali.belhacene@yahoo.fr>
2022-12-24 14:08:23 +0100

    update Domain entity abstraction

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -1,10 +1,12 @@
 import { v4 as createUUIDv4 } from 'uuid';
 
-export abstract class Entity<T> {
-  protected readonly id?: string | number;
-  protected properties: T;
-  constructor(properties: T, id?: string | number) {
+export abstract class Entity {
+  protected readonly id?: string;
+  constructor(id?: string) {
     this.id = id ? id : createUUIDv4();
-    this.properties = properties;
+  }
+
+  getId(): string {
+    return this.id;
   }
 }
