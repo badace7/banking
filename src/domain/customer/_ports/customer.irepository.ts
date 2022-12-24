16e3b494ff1bfd24f51992006af13d6f2c35cbbb
badace <ali.belhacene@yahoo.fr>
2022-12-24 14:18:50 +0100

    add customer business module domain

---
+++ src/domain/customer/_ports/customer.irepository.ts
@@ -0,0 +1,10 @@
+import CustomerDomain from '../domain/customer.domain';
+
+export interface ICustomerRepository {
+  findCustomerByAccountNumber(accountNumber: string): Promise<CustomerDomain>;
+  saveCustomer(customer: CustomerDomain): Promise<void>;
+  updateCustomer(
+    accountNumber: string,
+    customer: CustomerDomain,
+  ): Promise<void>;
+}
---
+++ src/domain/customer/domain/customer.domain.ts
@@ -0,0 +1,50 @@
+import { Entity } from '../../../core/domain/Entity';
+
+type CustomerProperties = {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  accountNumber: string;
+};
+
+class CustomerDomain extends Entity {
+  private firstName: string;
+  private lastName: string;
+  private accountNumber: string;
+  private constructor({
+    id,
+    firstName,
+    lastName,
+    accountNumber,
+  }: CustomerProperties) {
+    super(id);
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.accountNumber = accountNumber;
+  }
+
+  public getlastName(): string {
+    return this.lastName;
+  }
+
+  public setName(lastName: string): void {
+    this.lastName = lastName;
+  }
+  public getFirstName(): string {
+    return this.firstName;
+  }
+
+  public setFirstName(firstName: string): void {
+    this.firstName = firstName;
+  }
+
+  public getAccountNumber(): string {
+    return this.accountNumber;
+  }
+
+  public static create(customerData: CustomerProperties): CustomerDomain {
+    return new CustomerDomain(customerData);
+  }
+}
+
+export default CustomerDomain;
