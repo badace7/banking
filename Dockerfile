3ed79761c93184c2190d7758a7e0e0ee4fd11d2f
badace <ali.belhacene@yahoo.fr>
2023-01-08 19:28:38 +0100

    add COPY package-lock.json in Dockerfile and change npm install to npm ci

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM node:16.14.0-alpine as base
 WORKDIR /usr/app
 COPY package.json ./
-RUN npm install
+COPY package-lock.json ./
+RUN npm ci
 COPY . ./
 
 FROM base as test
---
+++ src/infrastructure/http/transaction/transaction.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, HttpStatus, Inject, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { ITransferRequest } from 'src/domain/transaction/_ports/input/transfer.irequest';
+
+@Controller('transaction')
+export class TransactionController {
+  constructor(
+    @Inject('ITransferRequest')
+    private readonly MoneyTransfer: ITransferRequest,
+  ) {}
+
+  @Get()
+  async getHello(@Res() response: Response) {
+    return response.status(HttpStatus.OK).send('hello');
+  }
+}
