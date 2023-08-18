01c8fd5d4558a04f689b28291a9f64d05ce1296a
badace7 <ali.belhacene@yahoo.fr>
2023-08-18 23:03:10 +0200

    update folders structure for better clarity

---
+++ src/modules/banking/infra/driven/postgres/operation.postgres.adapter.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { OperationEntity } from '../entities/operation.entity';
+import { EntityManager } from 'typeorm';
+
+import { Operation } from '../../../domain/operation';
+import { IOperationPort } from '../../../application/_ports/repositories/operation.iport';
+import { OperationMapper } from '../mappers/operation.mapper';
+
+@Injectable()
+export class OperationPostgresAdapter implements IOperationPort {
+  constructor(private readonly manager: EntityManager) {}
+  async getAllByAccountNumber(accountNumber: string): Promise<Operation[]> {
+    const operations = await this.manager
+      .createQueryBuilder(OperationEntity, 'operation')
+      .innerJoinAndSelect('operation.account', 'account')
+      .innerJoinAndSelect('operation.operationType', 'operationType')
+      .innerJoinAndSelect('operation.flowIndicator', 'flowIndicator')
+      .orderBy('operation.date', 'DESC')
+      .where('account.number = :number', { number: accountNumber })
+      .getMany();
+
+    return operations.map((operation) => OperationMapper.toDomain(operation));
+  }
+  async save(operation: Operation): Promise<void> {
+    const entity = OperationMapper.toEntity(operation);
+    await this.manager.save(OperationEntity, entity);
+  }
+}
