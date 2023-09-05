21ad1ebede054db37a70e12096efb35db05edd66
badace7 <ali.belhacene@yahoo.fr>
2023-09-05 17:13:16 +0200

    add conditional to restrict command and query request

--- src/libs/guards/ressource-owner.guard.ts
+++ src/libs/guards/ressource-owner.guard.ts
@@ -5,7 +5,9 @@ export class RessourceOwnerGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    const submittedAccountNumber = request.params.accountNumber;
+    const submittedAccountNumber =
+      request.params.accountNumber || request.body.origin;
+
     const isRessourceOwner = user.accountNumber === submittedAccountNumber;
 
     if (!isRessourceOwner) {
