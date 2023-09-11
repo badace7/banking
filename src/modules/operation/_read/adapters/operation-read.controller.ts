5f4c4b26d1abddd15cdfb12d87d00d523e3f8ea9
badace7 <ali.belhacene@yahoo.fr>
2023-09-11 18:12:50 +0200

    add guards to OperationReadController

--- src/modules/operation/_read/adapters/operation-read.controller.ts
+++ src/modules/operation/_read/adapters/operation-read.controller.ts
@@ -5,6 +5,7 @@ import {
   Inject,
   Param,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { Response } from 'express';
 import {
@@ -12,12 +13,19 @@ import {
   IGetOperationByAccountNumber,
 } from '../core/get-operations-by-account-number.iport';
 import { GetOperationsByNumberQuery } from '../core/get-operations-by-account-number.query';
+import { Roles } from 'src/libs/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/libs/guards/jwt.guard';
+import { RessourceOwnerGuard } from 'src/libs/guards/ressource-owner.guard';
+import { RolesGuard } from 'src/libs/guards/roles.guard';
+import { RoleEnum } from 'src/modules/authentication/core/domain/user';
 
 export enum OperationRead {
   ROOT = 'operations',
   OPERATIONS = ':accountNumber',
 }
 
+@UseGuards(JwtAuthGuard, RolesGuard, RessourceOwnerGuard)
+@Roles(RoleEnum.CUSTOMER)
 @Controller(OperationRead.ROOT)
 export class OperationReadController {
   constructor(
